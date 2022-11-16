136821721bc0581086de7fdb044bd7be3b9a7810
bmokase3093 <bmokase3093@gmail.com>
2022-11-16 11:37:01 +0200

    efs mount test

--- scripts/efs-mount.sh
+++ scripts/efs-mount.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-EC2=ec2-3-86-33-23.compute-1.amazonaws.com
-FS_ID=fs-07451f8ab8d7152f2.efs.us-east-1.amazonaws.com
+EC2="ec2-3-86-33-23.compute-1.amazonaws.com"
+FS_ID="fs-07451f8ab8d7152f2.efs.us-east-1.amazonaws.com"
 
 
 # efs-mount-point=/efs
@@ -11,5 +11,6 @@ sudo yum -y install nfs-utils
 sudo mkdir efs
 
 sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport $FS_ID:/   ~/efs
+# sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-07451f8ab8d7152f2.efs.us-east-1.amazonaws.com:/   ~/efs
 
-echo "sudo mount -t efs ${FS_ID}:/ /${efs-mount-point}" >> /etc/fstab
+ echo "sudo mount -t efs ${FS_ID}:/ ~/efs" >> /etc/fstab
