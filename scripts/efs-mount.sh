bbe29806292f1dc6a6399c18e7e85ab258de917e
bmokase3093 <bmokase3093@gmail.com>
2022-11-16 13:42:07 +0200

    test efs

--- scripts/efs-mount.sh
+++ scripts/efs-mount.sh
@@ -12,5 +12,6 @@ mkdir /var/www/html/WordPress/efs
 
 sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport $FS_ID:/   /var/www/html/WordPress/efs
 # sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-07451f8ab8d7152f2.efs.us-east-1.amazonaws.com:/   ~/efs
+echo "${FS_ID}:/ /var/www/html/WordPress/efs nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport,_netdev 0 0" >> /etc/fstab
 
- echo "sudo mount -t efs ${FS_ID}:/ /var/www/html/WordPress/efs" >> /etc/fstab
+# echo "sudo mount -t efs ${FS_ID}:/ /var/www/html/WordPress/efs" >> /etc/fstab
