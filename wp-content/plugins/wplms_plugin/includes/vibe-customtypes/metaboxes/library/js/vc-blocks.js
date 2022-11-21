b9f48e2219898ddd409d73de75514c4049f42711
Percy <percymohale9@gmail.com>
2022-11-21 21:12:43 +0200

    test

--- wp-content/plugins/wplms_plugin/includes/vibe-customtypes/metaboxes/library/js/vc-blocks.js
+++
@@ -1,7 +0,0 @@
-jQuery( 'body' ).delegate( '.radio_images', 'click', function(){
-    var value = jQuery(this).attr('data-value');
-    jQuery(this).parent().find('.clicked').removeClass('clicked');
-    jQuery(this).parent().find('.image_value').val(value); 
-    jQuery(this).addClass('clicked');
-    jQuery(this).append('<span></span>');
-});
